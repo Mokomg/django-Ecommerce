d75e64816feee32ab00d840c69dd5513a51bd1f0
mokom <mokomgeh45@gmail.com>
2021-05-05 18:36:50 -0400

    Project Setup & Store app created

---
+++ store/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+class Category(models.Model):
+    name = models.CharField(_("Name"), max_length=255, db_index=True)
+    slug = models.SlugField(_("Slug"), max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name="product", verbose_name=_("Product"), on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, verbose_name=_("User"), on_delete=models.CASCADE, related_name="product_creator")
+    title = models.CharField(_("Title"), max_length=255)
+    author = models.CharField(_("Author"), max_length=255, default="admin")
+    description = models.TextField(_("Description"), blank=True)
+    image = models.ImageField(_("Image"), upload_to="images", height_field=None, width_field=None, max_length=None)
+    slug = models.SlugField(_("Slug"), max_length=255)
+    price = models.DecimalField(_("Price"), max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(_("In Stock"), default=True)
+    is_active = models.BooleanField(_("Active"), default=True)
+    created = models.DateTimeField(_("Created"), auto_now_add=True)
+    updated = models.DateTimeField(_("Updated"), auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Products"
+        ordering = ("-created", "title")
+
+    def __str__(self):
+        return self.title
