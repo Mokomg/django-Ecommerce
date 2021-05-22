514e7eda2747d5817e6bfa806a1f1bc34e9e8217
mokom <mokomgeh45@gmail.com>
2021-05-22 15:40:29 -0400

    setup static folder

--- config/settings.py
+++ config/settings.py
@@ -94,13 +94,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
+# Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
